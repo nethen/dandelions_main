b5ed1d0ccdaff16b3c91b6b1c575d65f0dcece2c
nethen <72537484+nethen@users.noreply.github.com>
2022-11-01 00:06:50 -0700

    Added functionality for server to load squares

--- public/scripts/server.js
+++ public/scripts/server.js
@@ -1,3 +1,15 @@
+class SquareHolder {
+	constructor (x, y){
+		this.position = {x: x, y: y};
+		this.state = Math.floor(Math.random() * 5);
+	}
+
+
+}
+
+let tempSquares = [];
+const SQUARES = 100;
+
 const http = require('http')
 const express = require('express')
 
@@ -11,6 +23,12 @@ server.on('listening', () => {
  console.log('Listening on port 3000')
 })
 
+for (let i = 0; i < SQUARES; i++){
+    for (let j = 0; j < SQUARES; j++){
+    	tempSquares.push(new SquareHolder(i * 32,j * 32));
+    }
+  }
+
 // // Web sockets
 // const io = require('socket.io')(server,{
 //     allowEIO3: true // false by default
@@ -21,6 +39,13 @@ const io = require('socket.io')(server)
 
 io.sockets.on('connection', (socket) => {
 	console.log('Client connected: ' + socket.id)
+	//console.log(tempSquares)
+	// socket.on('squareRequest', () => {
+	// 	io.emit('squareRequest',tempSquares);
+	// });
+	io.emit('squareRequest', tempSquares);
+	
+	//socket.emit('squares',tempSquares);
 	socket.on('mouse', (data) => socket.broadcast.emit('mouse', data))
 
 	socket.on('disconnect', () => console.log('Client has disconnected'))
--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,47 +1,56 @@
 p5.disableFriendlyErrors = true; // disables FES
 
 const SQUARE_SIZE = 2;
+const IMG_SIZE = 32;
 const SQUARE_COUNT = 16;
 const CANVAS_COUNT = 100;
 const MOVING_COUNT = 20;
-let timeFreeze = true;
 let socket;
 
 class Square {
-  constructor(x,y, bool){
-    this.x = x;
-    this.y = y;
-    this.randomSize = random();
-    if (this.randomSize < 0.3) {
-      this.counter = SQUARE_SIZE;
-      this.minCounter = SQUARE_SIZE;
-      this.maxCounter = SQUARE_SIZE * 4;
+  constructor(x,y, state){
+    this.position = {x: x, y: y};
+    this.globalPos = {x: x, y: y}
+    this.state = state;
+    this.counter = this.state;
+    if (this.counter === 32){
+      this.maxCounter = 32;
+    } else{
+      this.maxCounter = this.counter * 2;
     }
-    else if (this.randomSize < 0.6){
-      this.counter = SQUARE_SIZE*2;
-      this.minCounter = SQUARE_SIZE*2;
-      this.maxCounter = SQUARE_SIZE * 8;
+
+    if (this.counter === 2){
+      this.minCounter = 2;
+    } else{
+      this.minCounter = this.counter / 2;
     }
-    else{
-      this.counter = SQUARE_SIZE*4;
-      this.minCounter = SQUARE_SIZE*4;
-      this.maxCounter = SQUARE_SIZE * SQUARE_COUNT;
-    } 
     this.counterB = 100;
     this.adder = 1;
-    this.moving = bool;
+    // this.moving = bool;
+    this.active = false;
+    this.currImg = img;
   }
   
+
   display(){
     push();
-    translate(this.x, this.y);
-    image(img,0,0,SQUARE_SIZE*SQUARE_COUNT,SQUARE_SIZE*SQUARE_COUNT,0,0,this.counter, this.counter)
+    translate(this.position.x, this.position.y);
+    image(this.currImg,0,0,IMG_SIZE,IMG_SIZE,0,0,this.counter, this.counter)
 
     pop();
   }
   
+  updateImg(){
+    if (this.currImg === img) this.currImg = img2;
+    else this.currImg = img;
+  }
+
+  updateMoving(){
+    this.moving = !this.moving;
+  }
+
   update(){
-    if (this.moving == true && timeFreeze){
+    if (this.moving == true){
       if (this.counter < this.minCounter || this.counter > this.maxCounter){
         this.adder *= -1;
         this.counter += this.adder;
@@ -58,17 +67,22 @@ let squares = []
 
 function preload() {
  img = loadImage("assets/asset.png");
+ img2 = loadImage("assets/asset2.png");
 }
 
 function setup() {
   socket = io.connect('http://localhost:3000')
-
+  socket.on('squareRequest',(x) => {
+    x.forEach(function(square){
+      squares.push(new Square(square.position.x, square.position.y, Math.pow(2,1 + square.state)));
+    });
+    // for (let i = 0; i < CANVAS_COUNT; i++){
+    //   for (let j = 0; j < CANVAS_COUNT; j++){
+    //   squares.push(new Square(x[],j * SQUARE_SIZE*SQUARE_COUNT, i<MOVING_COUNT && j < MOVING_COUNT));
+    //   }
+    // }
+  });
   createCanvas(SQUARE_SIZE*SQUARE_COUNT*CANVAS_COUNT, SQUARE_SIZE*SQUARE_COUNT*CANVAS_COUNT);
-  for (let i = 0; i < CANVAS_COUNT; i++){
-    for (let j = 0; j < CANVAS_COUNT; j++){
-    squares.push(new Square(i * SQUARE_SIZE*SQUARE_COUNT,j * SQUARE_SIZE*SQUARE_COUNT, i<MOVING_COUNT && j < MOVING_COUNT));
-    }
-  }
   noSmooth();
 }
 
@@ -81,5 +95,11 @@ function draw() {
 }
 
 function mouseClicked() {
-  timeFreeze = !timeFreeze;
+  const active = (element) => (element.position.x < mouseX && element.position.x + IMG_SIZE > mouseX) && (element.position.y < mouseY && element.position.y + IMG_SIZE > mouseY);
+
+  console.log(squares.find(active));
+  const x = squares.find(active);
+  x.updateImg();
+  x.updateMoving();
+
 }
