8fcb8527cf3b83c1578edef2e80a5c9c2223c6d3
nethen <72537484+nethen@users.noreply.github.com>
2022-11-24 01:28:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let rippleSquares =[];
 let placeholders = [];
 
 //Size of canvas
-const SQUARES = 28;
+const SQUARES = 64;
 
 //Set up connection
 const http = require('http')
@@ -102,7 +102,7 @@ setInterval(function() {
 io.sockets.on('connection', (socket) => {
 	console.log('Client connected: ' + socket.id)
 	//console.log(updatePlaceable());
-	socket.emit('pageLoad', [{x: Math.floor(Math.random()*(3)), y: Math.floor(Math.random()*(3))},loadSquares,placeholders]);
+	socket.emit('pageLoad', [{x: Math.floor(Math.random()*((SQUARES-1)/9)), y: Math.floor(Math.random()*((SQUARES-1)/9))},loadSquares,placeholders]);
 	//socket.emit('pRequest', placeholders);
 
 	socket.on('mouse', (data) => socket.broadcast.emit('mouse', data))
